5d30152f70f375dec2d5daa4782a867c4322195c
Robert Bikar <rbikar@redhat.com>
2022-03-13 13:43:41 +0100

    Various fixes #3
    - removed unused files
    - bumped version of bulma css

--- app/static/season.js
+++
@@ -1,8 +0,0 @@
-const select = document.querySelector('.select'); //ak jine id
-//const navbarMenu = document.querySelectselecor('#navbarMenuHeroA');
-
-select.addEventListener('click', () => {
-    //navbarMenu.classList.toggle('is-active')
-    console.log("asdf")
-})
-
