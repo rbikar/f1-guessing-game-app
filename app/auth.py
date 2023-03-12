a56d608fa2da8bec0efb86f70e0d83ff2bd509c5
rbikar <31000668+rbikar@users.noreply.github.com>
2023-03-12 23:26:50 +0100

    Update auth.py

--- app/auth.py
+++ app/auth.py
@@ -52,7 +52,7 @@ def signup_post():
         password = request.form.get("password")
         pin = request.form.get("pin")
 
-        if str(pin) != str(os.getenv("PIN", "1234")):
+        if str(pin) != str(os.getenv("PIN", "CHANGE-ME")):
             flash("Neplatný PIN")
             return redirect(url_for("auth.signup"))
         user = User.query.filter_by(
