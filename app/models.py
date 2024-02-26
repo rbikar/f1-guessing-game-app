c560d54e67ab48cd1f051f7382025512ea124552
Robert Bikar <rbikar@redhat.com>
2024-02-26 22:15:58 +0100

    Update User model
    * set bigger length for password row

--- app/models.py
+++ app/models.py
@@ -10,7 +10,7 @@ class User(UserMixin, db.Model):
     id = db.Column(
         db.Integer, primary_key=True
     )  # primary keys are required by SQLAlchemy
-    password = db.Column(db.String(100))
+    password = db.Column(db.String(400))
     username = db.Column(db.String(100), unique=True)
     role = db.Column(db.String(100))
 
